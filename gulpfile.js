adb511c09d7f08d86ed2c7c13139c1d5d674aab6
jimmyLi <liviu.c.simion@gmail.com>
2016-04-12 13:35:00 +0200

    installed node-normalize-scss

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,14 @@
  // grab our gulp packages
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 // create tasks
 gulp.task('sass', function () {
   return gulp.src('./source/scss/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./public/assets/css'));
 });
  
