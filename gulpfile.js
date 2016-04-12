eb91c74581606a1e79b868f99579ed2dc8fe23fd
jimmyLi <liviu.c.simion@gmail.com>
2016-04-12 13:03:21 +0200

    installed gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
-
 /* File: gulpfile.js */
 
-// grab our gulp packages
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+'use strict';
 
-// create a default task and just log a message
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+ // grab our gulp packages
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// create tasks
+gulp.task('sass', function () {
+  return gulp.src('./source/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/assets/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
