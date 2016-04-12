3aeeb54cb10d096de1b2ba2eaeadcfab51b263d4
jimmyLi <liviu.c.simion@gmail.com>
2016-04-12 13:15:05 +0200

    installed gulp sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var sass = require('gulp-sass');
 // create tasks
 gulp.task('sass', function () {
   return gulp.src('./source/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/assets/css'));
 });
